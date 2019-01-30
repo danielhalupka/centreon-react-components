8d1cdc6ad9e88a1ca37a4783b68e2898a701cad3
Danijel Chalupka <danijel@simple-task.com>
2019-01-30 22:25:56 +0100

    upload finished and prettier run over code

--- src/FileUpload/FileUploadItem.js
+++ src/FileUpload/FileUploadItem.js
@@ -17,23 +17,29 @@ class FileUploadItem extends Component {
     return (
       <React.Fragment>
         <div className="file-upload-item">
-          {icon ? <span className={`file-upload-item-icon ${icon} ${iconStatus}`} /> : null}
-          <span className={`file-upload-item-title ${titleStatus}`}>{title}</span>
-          {info ? <span className={`file-upload-item-info ${infoStatus}`}>
-            {infoStatusLabel}
-            {info}
-          </span> : null}
-          {
-            !uploading ? (<span className="icon-close icon-close-small" onClick={onDeleteFile} />) : null
-          }
-          {
-            progressBar ? (
-              <div className="progress">
-                <span className={`progress-bar ${progressBar}`} />
-              </div>
-            ) : null
-          }
-
+          {icon ? (
+            <span className={`file-upload-item-icon ${icon} ${iconStatus}`} />
+          ) : null}
+          <span className={`file-upload-item-title ${titleStatus}`}>
+            {title}
+          </span>
+          {info ? (
+            <span className={`file-upload-item-info ${infoStatus}`}>
+              {infoStatusLabel}
+              {info}
+            </span>
+          ) : null}
+          {!uploading ? (
+            <span
+              className="icon-close icon-close-small"
+              onClick={onDeleteFile}
+            />
+          ) : null}
+          {progressBar ? (
+            <div className="progress">
+              <span className={`progress-bar ${progressBar}`} />
+            </div>
+          ) : null}
         </div>
       </React.Fragment>
     );
--- src/FileUpload/index.js
+++ src/FileUpload/index.js
@@ -2,38 +2,42 @@ import React, { Component } from "react";
 import Button from "../Button/ButtonRegular";
 import Popup from "../Popup";
 import FileUploadItem from "./FileUploadItem";
-import FileUploadProgress from './FileUploadProgress';
+import FileUploadProgress from "./FileUploadProgress";
 import "./file-upload.scss";
-import Files from 'react-files'
+import Files from "react-files";
 
 class FileUpload extends Component {
-
   state = {
-    uploading: false,
     files: []
-  }
+  };
 
-  onFilesChange = (files) => {
+  onFilesChange = files => {
     this.setState({
       files
-    })
+    });
   };
 
-  onFilesError = (error, file) => {
-    console.log('error code ' + error.code + ': ' + error.message)
+  onFilesError = error => {
+    console.log("error code " + error.code + ": " + error.message);
   };
 
-  onRemoveFile = (idx) => {
-    let {files} = this.state;
+  onRemoveFile = idx => {
+    let { files } = this.state;
     files.splice(idx, 1);
     this.setState({
       files
-    })
-  }
+    });
+  };
 
   render() {
-    const { files, uploading } = this.state;
-    const { onClose } = this.props;
+    const { files } = this.state;
+    const {
+      onClose,
+      uploading,
+      onApply,
+      uploadingProgress,
+      finished
+    } = this.props;
     return (
       <React.Fragment>
         <Popup popupType="small">
@@ -42,14 +46,15 @@ class FileUpload extends Component {
               <div className="container__col-xs-6 center-vertical">
                 <div className="file file-upload">
                   <span className="file-upload-title">
-                    <span className="file-upload-icon" />File Upload
+                    <span className="file-upload-icon" />
+                    File Upload
                   </span>
                 </div>
               </div>
               <Files
                 onChange={this.onFilesChange}
                 onError={this.onFilesError}
-                accepts={['.zip', '.license']}
+                accepts={[".zip", ".license"]}
                 multiple
                 maxFiles={5}
                 maxFileSize={1048576}
@@ -63,43 +68,59 @@ class FileUpload extends Component {
             </div>
             <span className="icon-close icon-close-middle" onClick={onClose} />
           </div>
-          {
-            files.length > 0 ? (
-              <div className="popup-body">
-                <div className="file file-upload">
-                  <div className="file-upload-items">
-                    {
-                      files.map((file, idx) => (
-                        <FileUploadItem
-                          icon="file"
-                          iconStatus="warning"
-                          title={file.name}
-                          titleStatus="warning"
-                          info={file.sizeReadable}
-                          onDeleteFile={()=>{this.onRemoveFile(idx)}}
-                          uploading={uploading}
-                        />
-                      ))
-                    }
-                    {
-                      uploading ? (
-                        <FileUploadProgress
-                          title="Progress"
-                          titleStatus="percentage"
-                          progressBar='percentage'
-                          uploadedPercentage='70' />
-                      ) : null
-                    }
-
-                  </div>
+          {files.length > 0 ? (
+            <div className="popup-body">
+              <div className="file file-upload">
+                <div className="file-upload-items">
+                  {files.map((file, idx) => (
+                    <FileUploadItem
+                      icon="file"
+                      iconStatus="warning"
+                      title={file.name}
+                      titleStatus="warning"
+                      info={file.sizeReadable}
+                      onDeleteFile={() => {
+                        this.onRemoveFile(idx);
+                      }}
+                      uploading={uploading}
+                    />
+                  ))}
+                  {uploading ? (
+                    <FileUploadProgress
+                      title="Progress"
+                      titleStatus="percentage"
+                      progressBar="percentage"
+                      uploadedPercentage={uploadingProgress}
+                    />
+                  ) : null}
                 </div>
+                {uploading && finished ? (
+                  <Button
+                    label={"Ok"}
+                    buttonType="regular"
+                    customClass="mr-2 float-right"
+                    color="green"
+                    onClick={onClose}
+                  />
+                ) : null}
+                {!uploading ? (
+                  <Button
+                    label={"Apply"}
+                    buttonType="regular"
+                    customClass="mr-2 float-right"
+                    color="green"
+                    onClick={() => {
+                      onApply(files);
+                    }}
+                  />
+                ) : null}
               </div>
-            ) : null
-          }
+            </div>
+          ) : null}
         </Popup>
       </React.Fragment>
     );
   }
-};
+}
 
 export default FileUpload;
