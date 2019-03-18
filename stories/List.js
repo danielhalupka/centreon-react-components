c4a2abff554c505b64b9282df51361517d13555e
Nikola Lovic <nikola.lovic@simple-task.com>
2019-03-18 15:54:33 +0100

    Import Redux and sagas to the storybook.

--- stories/List.js
+++ stories/List.js
@@ -1,11 +1,17 @@
 import React from "react";
-import { storiesOf } from "@storybook/react";
-import { ListSortable } from "../src";
+import {storiesOf} from "@storybook/react";
+import {ListSortable} from "../src";
+import Provider from '../Provider.js'
+import configureStore from '../configureStore.js'
 
-storiesOf("List", module).add(
-  
-  "List - sortable",
-  () => 
-  <ListSortable />,
-  { notes: "A very simple component" }
-);
+const store = configureStore;
+
+const withProvider = (story) => (
+  <Provider store={store}>
+    {story()}
+  </Provider>
+)
+
+storiesOf("List", module)
+  .addDecorator(withProvider)
+  .add("List - sortable", () => <ListSortable/>, {notes: "A very simple component"});
