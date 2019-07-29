7df7310e459c0dd8df43383d932e085ebfed2cde
Bruno d'Auria <bdauria@centreon.com>
2019-07-29 12:27:19 +0200

    Write snapshot test for ErrorDialog

---
+++ src/MaterialComponents/Dialog/Error/index.test.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import { render } from '@testing-library/react';
+import ErrorDialog from '.';
+
+describe('ErrorDialog', () => {
+  it('renders', () => {
+    const { container } = render(
+      <ErrorDialog
+        open
+        title="Error"
+        errorText="Something unexpected happened..."
+        confirmLabel="Close"
+        onClose={() => {}}
+      />,
+    );
+
+    expect(container.firstChild).toMatchSnapshot();
+  });
+});
